5af52dfc126f48ca80e1b5055d8049ec6a27a413
Bhagwan Kumar Singh <117030732+Bhagwankumarsingh@users.noreply.github.com>
2023-01-02 22:40:12 +0530

    Create combination_sum_III.cpp

---
+++ combination_sum_III.cpp
@@ -0,0 +1,29 @@
+// leetcode -> (216).
+
+class Solution {
+    private:
+    void solve(int idx,int sum,int n,vector<vector<int>>&ans,vector<int>&ans2,int k){
+        if(sum==n && k==0){
+            ans.push_back(ans2);
+            return ;
+        }
+        if(sum>n){
+            return;
+        }
+        for(int i=idx;i<=9;i++){
+            if(i>n){
+                break;
+            }
+            ans2.push_back(i);
+            solve(i+1,sum+i,n,ans,ans2,k-1);
+            ans2.pop_back();
+        }
+    }
+public:
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vector<vector<int>>ans;
+        vector<int>ans2;
+        solve(1,0,n,ans,ans2,k);
+        return ans;
+    }
+};
